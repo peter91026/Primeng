e128a842c26707ed2a567fcd23d65f31c2e27ac4
peter91026 <aa815860@gmail.com>
2023-03-26 01:31:36 +0800

    table顯示不出來

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
+interface work{
+  work:string
+  code:string
+}
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
   styleUrls: ['./survey.component.scss']
+
 })
 export class SurveyComponent {
+  selectedCategories: any[] = [];
+  categories: any[] = [
+    { name: 'Angular', key: 'A' },
+    { name: 'Bonita', key: 'B' },
+    { name: 'Database', key: 'D' },
+    { name: 'Golang', key: 'G' }
+  ];
 
-}
+  Profession:work[]=[];
+  selectedProfession:work[]=[];
+  ngOnInit(){
+    this.Profession=[
+      {work:'學生',code:'S'},
+      {work:'上班族',code:'O'},
+      {work:'軍人',code:'SO'},
+      {work:'老師',code:'T'},
+    ];
+
+    }
+  }
+
+
